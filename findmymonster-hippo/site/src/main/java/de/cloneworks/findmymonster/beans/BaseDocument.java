3770fadc1d96ba6fb6b4f44f8316f4ddb2254860
Jan Pralle <jan.pralle@netpioneer.de>
2018-01-30 09:30:03 +0100

    Initial version with empty hippo and empty angular CLI app.

---
+++ findmymonster-hippo/site/src/main/java/de/cloneworks/findmymonster/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package de.cloneworks.findmymonster.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="findmymonster:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
